38d144006517d829b37458d17d62afbe987d2626
João Victor de Oliveira <60987160+joaovictoroli@users.noreply.github.com>
2022-09-23 18:25:06 -0300

    Ponta Pé
    Integração com banco de dados

---
+++ aspnet-blog-web/aspnet-blog-web/Data/BlogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using aspnet_blog_web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnet_blog_web.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
